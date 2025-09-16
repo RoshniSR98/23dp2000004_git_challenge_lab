0c7b964810956ab9a29b4c14b076ba4f18a2c349
Roshni SR <roshniqrs@gmail.com>
2025-09-16 20:03:18 +0530

    updated main.py to use all modules

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from sum import *
+from difference import *
+from product import *
+from division import *
+
 print("Arithmetic Project Started")
+
+a, b = 10, 5
+
+print("Sum:", add(a, b))
+print("Difference:", subtract(a, b))
+print("Product:", multiply(a, b))
+print("Division:", divide(a, b))
